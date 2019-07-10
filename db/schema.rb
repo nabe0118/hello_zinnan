4b336a8c3c33c1f1c816b3fa10e45e7954431245
hirotobb7 <hiroto.bb7.pf@gmail.com>
2019-07-10 06:29:59 +0000

    調整

--- db/schema.rb
+++ db/schema.rb
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2019_07_09_083233) do
     t.integer "artist_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -80,7 +81,5 @@ ActiveRecord::Schema.define(version: 2019_07_09_083233) do
     t.string "phone_number", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   end
-
 end
