ae53584ef813e862aa82a5df686ac8da8bdf2b1c
nabe0118 <watahen0118@gmail.com>
2019-07-24 10:49:21 +0000

    order

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,7 @@
 class Address < ApplicationRecord
 
 belongs_to :user
+has_many :orders
 validates :sei, :mei, format: { with: /\A[ァ-ヶー－]+\z/ , message: "は全角カタカナで入力してください"}
 validates :postal_code, format: { with: /\A\d{3}[-]\d{4}$|^\d{3}[-]\d{2}$|^\d{3}$|^\d{5}$|^\d{7}\z/ , message: "が正しく入力されていません"}
 validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ , message: "が正しく入力されていません"}
