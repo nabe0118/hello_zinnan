31c690c49a2f9dc4fdc36175a5089391b98d6810
nabe0118 <watahen0118@gmail.com>
2019-07-25 07:26:13 +0000

    add

--- app/controllers/administrator/orders_controller.rb
+++ app/controllers/administrator/orders_controller.rb
@@ -5,11 +5,19 @@ class Administrator::OrdersController < ApplicationController
   end
 
   def show
-  @orders = Order.all
   @order = Order.find(params[:id])
+  @order_details = @order.order_details
   end
 
   def edit
+  @order = Order.find(params[:id])
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    @order.update
+      flash[:notice] = "編集完了しました。"
+    redirect_to administrator_order_path
   end
 
 
